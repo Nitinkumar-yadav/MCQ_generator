9a05e044c7df4d1a29458f9c1e67289caaa88930
Nitinkumar Yadav <126796871+Nitinkumar-yadav@users.noreply.github.com>
2023-07-13 20:53:12 +0530

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
+from pydantic_core import BaseModel
 
 
 app =FastAPI()
